c32c9d8c9a4c5933b2f0b8a71b2f54d27dcd8e7c
Leo Gza <leo_gza98@hotmail.com>
2023-12-01 11:51:37 -0600

    M

---
+++ ManejoPresupuesto/Models/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "El Campo {0} es requerido")]
+        [EmailAddress(ErrorMessage = "El Campo debe ser un correo electronico valido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "El Campo {0} es requerido")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public bool Recuerdame { get; set; }
+    }
+}
---
+++ ManejoPresupuesto/Models/RegistroViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
+{
+    public class RegistroViewModel
+    {
+        [Required(ErrorMessage = "El Campo {0} es requerido")]
+        [EmailAddress(ErrorMessage = "El Campo debe ser un correo electronico valido")]
+        public string Email { get; set; }
+        
+        [Required(ErrorMessage = "El Campo {0} es requerido")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
